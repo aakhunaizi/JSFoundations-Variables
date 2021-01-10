1ddd08029ecc1a419af90f4d1e9177ad609e05df
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-01-10 12:34:25 +0300

    Added story.js

---
+++ story.js
@@ -0,0 +1,9 @@
+const name = "Ahmed";
+const age = 22;
+const hobby = "watching cat videos on the internet";
+
+console.log(
+  `Mr./Mrs. ${name}, in 33 years you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
