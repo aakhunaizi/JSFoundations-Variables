b07a905a1d5969a1e9ed9fc70cd3c1cf512341e6
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-01-10 14:09:25 +0300

    Edited story.js

--- story.js
+++ story.js
@@ -3,7 +3,7 @@ const age = 22;
 const hobby = "watching cat videos on the internet";
 
 console.log(
-  `Mr./Mrs. ${name}, in 33 years you will be ${
+  `Mr. ${name}, in 33 years you will be ${
     age + 33
   } years old. By then you'll have decades of ${hobby} practice!`
 );
